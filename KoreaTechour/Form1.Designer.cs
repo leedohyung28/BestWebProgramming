8c52b2df99a4ce8aaedb462546cc5b1eee6780a4
SepYoKim6745 <gkfhak123@gmail.com>
2023-10-31 10:25:14 +0900

    ADD FrameWork

--- KoreaTechour/Form1.Designer.cs
+++ KoreaTechour/Form1.Designer.cs
@@ -29,24 +29,21 @@ namespace KoreaTechour
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.metroContextMenu1 = new MetroFramework.Controls.MetroContextMenu(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // metroContextMenu1
             // 
-            this.button1.Location = new System.Drawing.Point(427, 180);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.metroContextMenu1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.metroContextMenu1.Name = "metroContextMenu1";
+            this.metroContextMenu1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,8 +51,7 @@ namespace KoreaTechour
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private MetroFramework.Controls.MetroContextMenu metroContextMenu1;
     }
 }
 
