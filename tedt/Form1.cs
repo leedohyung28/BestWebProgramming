381bf5b7186b91d795e5bd843bc516a36f1dcb26
KJH0476 <hyeok1234565@gmail.com>
2023-11-07 09:26:46 +0900

    start

---
+++ tedt/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace tedt
+{
+    public partial class Form1 : Form
+    {
+        private static string connectionString = "server=webp.flykorea.kr;user=w2019136007;database=w2019136007DB;port=13306;password=w2019136007;";
+        private MySqlConnection connection = new MySqlConnection(connectionString);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            connection.Open();
+
+            try
+            {
+                string id = textBox1.Text.ToString();
+                string pass = textBox2.Text.ToString();
+
+                if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(pass))
+                {
+                    throw new Exception("값을 입력해주세요.");
+                }
+
+                command.CommandText = "SELECT uid, upwd, uname, umajor, ugrade FROM USER WHERE uid=@id AND upwd=@password";
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@password", pass);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string name = reader["uname"].ToString();
+
+                    Form3 form3 = new Form3(id, name);
+                    form3.ShowDialog();
+                }
+                else
+                {
+                    throw new Exception("아이디 또는 비밀번호가 틀렸습니다.");
+                }
+
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message, "오류"); 
+            }
+            finally { connection.Close(); }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ tedt/Form2.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tedt
+{
+    public partial class Form2 : Form
+    {
+        private static string connectionString = "server=webp.flykorea.kr;user=w2019136007;database=w2019136007DB;port=13306;password=w2019136007;";
+        private MySqlConnection connection = new MySqlConnection(connectionString);
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            connection.Open();
+
+            try
+            {
+                if (textBox2.Text.ToString() != textBox3.Text.ToString()) throw new Exception("비밀번호를 확인해주세요");
+
+                string id = textBox1.Text.ToString();
+                MySqlCommand checkIdCommand = new MySqlCommand("SELECT COUNT(*) FROM USER WHERE uid = @id", connection);
+                checkIdCommand.Parameters.AddWithValue("@id", id);
+
+                int idCount = Convert.ToInt32(checkIdCommand.ExecuteScalar());
+                if (idCount > 0)
+                {
+                    throw new Exception("아이디가 이미 존재합니다.");
+                }
+
+                command.CommandText = "INSERT INTO USER (uid, upwd, uname, umajor, ugrade) VALUES (@id, @password, @name, @major, @grade)";
+                command.Parameters.AddWithValue("@id", textBox1.Text.ToString());
+                command.Parameters.AddWithValue("@password", textBox2.Text.ToString());
+                command.Parameters.AddWithValue("@name", textBox4.Text.ToString());
+                command.Parameters.AddWithValue("@major", textBox5.Text.ToString());
+                command.Parameters.AddWithValue("@grade", textBox6.Text.ToString());
+                command.ExecuteNonQuery();
+
+                this.Close();
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "오류");
+            }
+            finally { 
+                connection.Close();
+            }
+        }
+    }
+}
