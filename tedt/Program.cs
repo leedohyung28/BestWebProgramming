1e69c81b7170c8e3a68d727fcf8bcbee0a3f9d23
KimYoSep <gkfhak123@gmail.com>
2023-11-10 15:55:12 +0900

    run

--- tedt/Program.cs
+++ tedt/Program.cs
@@ -16,7 +16,7 @@ namespace tedt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form3());
+            Application.Run(new loginForm());
         }
     }
 }
